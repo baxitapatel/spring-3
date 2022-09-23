1e010740a8b694ef4c5196a919062539561ea6b6
baxitapatel <baxitapatel@gmail.com>
2022-09-22 20:32:12 -0400

    initial code

---
+++ src/test/java/io/pragra/learing/spring3session/Spring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pragra.learing.spring3session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
